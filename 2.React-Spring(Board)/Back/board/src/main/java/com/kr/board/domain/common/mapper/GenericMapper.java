73e0cc11fa6a3642e368082aec0ba2f010077ea4
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-08-23 13:08:44 +0900

    refactor: rebuild

--- 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/common/mapper/GenericMapper.java
+++ 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/common/mapper/GenericMapper.java
@@ -1,9 +1,5 @@
 package com.kr.board.domain.common.mapper;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
 import java.util.List;
 
 public interface GenericMapper <D, E>{
