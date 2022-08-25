b86f96b608cdef16c0bbc540ba1378951a4223f0
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-08-25 16:08:27 +0900

    refactor : MemberException 불변 객체로 변경 후 팩토리 메서드 패턴 적용

--- 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/member/error/MemberException.java
+++ 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/member/error/MemberException.java
@@ -3,13 +3,14 @@ package com.kr.board.domain.member.error;
 import lombok.*;
 
 @Getter
-@RequiredArgsConstructor
 public class MemberException extends RuntimeException{
     private MemberErrorResult memberErrorResult;
 
     private MemberException(MemberErrorResult memberErrorResult) {
         this.memberErrorResult = memberErrorResult;
     }
+
+    private MemberException(){}
 
     public static MemberException of(final MemberErrorResult memberErrorResult){
         return new MemberException(memberErrorResult);
