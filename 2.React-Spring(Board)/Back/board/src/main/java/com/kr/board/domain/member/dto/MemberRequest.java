0baf4c8cf52ac69ead828192e38759ece357fc5b
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-08-25 13:11:06 +0900

    refactor : MemberRequest 정규 표현식 적용

--- 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/member/dto/MemberRequest.java
+++ 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/member/dto/MemberRequest.java
@@ -1,9 +1,11 @@
 package com.kr.board.domain.member.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
@@ -12,7 +14,16 @@ import lombok.NoArgsConstructor;
 public class MemberRequest {
 
     private Long id;
+
+    @NotNull
+    @Email(regexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")
     private String email;
+
+    @NotNull
+    @Size(min = 3)
     private String nickname;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$")
     private String password;
 }
