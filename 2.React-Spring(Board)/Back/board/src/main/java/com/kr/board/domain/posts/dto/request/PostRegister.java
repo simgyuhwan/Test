57364d68c46a2166a72253f8a95acd5245deb0d1
sgh1939 <sgh1939@danusys.com>
2022-09-16 18:06:00 +0900

    feat : 개시글 등록 PostRegister에서 유효성 검증을 위한 ''@NotNull' 애너테이션 추가

--- 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/posts/dto/request/PostRegister.java
+++ 2.React-Spring(Board)/Back/board/src/main/java/com/kr/board/domain/posts/dto/request/PostRegister.java
@@ -5,14 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class PostRegister {
     private Long id;
+
+    @NotNull
     private String title;
+
+    @NotNull
     private String content;
+
     private String writer;
-    private Long viewCount;
 }
