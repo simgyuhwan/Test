35ebaf4c37ca27397c517afd9247c4dabe05c12f
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-12 20:55:52 +0900

    스프링 부트 테스트 추가 컨트롤러 추가

--- 3. Jpa/src/main/java/com/practice/jpa/product/ProductService.java
+++ 3. Jpa/src/main/java/com/practice/jpa/product/ProductService.java
@@ -1,16 +1,25 @@
 package com.practice.jpa.product;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/products")
 public class ProductService {
     private final ProductPort productPort;
 
     ProductService(final ProductPort productPort) {
         this.productPort = productPort;
     }
-    public void addProduct(AddProductRequest request) {
-        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
+    @PostMapping
+    public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
+        final Product product = new Product(request.name(), request.price(), request.discountPolicy());
+        productPort.save(product);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
