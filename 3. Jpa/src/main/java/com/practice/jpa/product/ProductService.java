53292242fae03a5fb7fc22962fb8081aa929b31b
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-03-16 23:17:43 +0900

    상품 조회 API 구현

--- 3. Jpa/src/main/java/com/practice/jpa/product/ProductService.java
+++ 3. Jpa/src/main/java/com/practice/jpa/product/ProductService.java
@@ -3,10 +3,7 @@ package com.practice.jpa.product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
@@ -25,12 +22,14 @@ public class ProductService {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    public GetProductResponse getProduct(long productId) {
+    @GetMapping("/{productId}")
+    public ResponseEntity<GetProductResponse> getProduct(@PathVariable final long productId) {
         Product product = productPort.getProduct(productId);
-        return new GetProductResponse(
+        GetProductResponse response = new GetProductResponse(
                 product.getId(),
                 product.getName(),
                 product.getPrice(),
                 product.getDiscountPolicy());
+        return ResponseEntity.ok(response);
     }
 }
--- 3. Jpa/src/test/java/com/practice/jpa/product/ProductApiTest.java
+++ 3. Jpa/src/test/java/com/practice/jpa/product/ProductApiTest.java
@@ -1,5 +1,8 @@
 package com.practice.jpa.product;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -16,6 +19,21 @@ class ProductApiTest extends ApiTest{
         final var response = ProductSteps.상품등록요청(request);
 
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품조회() {
+        productSteps.상품등록요청(ProductSteps.상품등록요청_생성());
+        Long productId = 1L;
+
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명");
     }
 
     private AddProductRequest 상품등록요청_생성() {
