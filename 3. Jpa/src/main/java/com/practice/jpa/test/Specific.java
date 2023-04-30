675833d3f8583d4c76d4f26d3dc32f6d4161ebf1
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-04-30 23:34:22 +0900

    테스트

--- 3. Jpa/src/main/java/com/practice/jpa/test/Specific.java
+++ 3. Jpa/src/main/java/com/practice/jpa/test/Specific.java
@@ -7,6 +7,38 @@ import org.springframework.util.StringUtils;
 
 public class Specific {
 	private static String[] EMPTY_STRING_ARRAY;
+	private static String SPACE = " ";
+
+	public static String leftPad(final String str, final int size, String padStr) {
+		if(str == null) return null;
+
+		if(padStr == null || padStr.isEmpty()) {
+			padStr = SPACE;
+		}
+
+		final int padLen = padStr.length();
+		final int strLen = str.length();
+		final int pads = size - strLen;
+
+		if(pads <= 0) {
+			return str;
+		}
+
+		if(pads == padLen) {
+			return padStr.concat(str);
+		}else if(pads < padLen) {
+			return padStr.substring(0, pads).concat(str);
+		}else {
+			final char[] padding = new char[pads];
+			final char[] padChars = padStr.toCharArray();
+
+			for(int i=0; i<pads; i++) {
+				padding[i] = padChars[i % padLen];
+			}
+
+			return new String(padding).concat(str);
+		}
+	}
 
 	public static String[] substringsBetween(final String str, final String open, final String close) {
 		if(isEmpty(str) || isEmpty(open) || isEmpty(close)) {
--- 3. Jpa/src/test/java/com/practice/jpa/test/SpecificTest.java
+++ 3. Jpa/src/test/java/com/practice/jpa/test/SpecificTest.java
@@ -1,8 +1,14 @@
 package com.practice.jpa.test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.*;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class SpecificTest {
 
@@ -20,4 +26,25 @@ class SpecificTest {
 	void openAndCloseTagsThatAreLongerThan1Char() {
 		assertThat(Specific.substringsBetween("aabcddaabfddaab", "aa","dd")).isEqualTo(new String[] {"bc", "bf"});
 	}
+
+	@ParameterizedTest
+	@MethodSource("generator")
+	void test(String originalStr, int size, String padString, String expectedStr) {
+		assertThat(Specific.leftPad(originalStr, size, padString))
+			.isEqualTo(expectedStr);
+	}
+
+	static Stream<Arguments> generator() {
+		return Stream.of(
+			of(null, 10, "-", null),
+			of("", 5, "-", "-----"),
+			of("abc", -1, "-", "abc"),
+			of("abc", 5, "", "  abc"),
+			of("abc", 5, "-", "--abc"),
+			of("abc", 3, "-", "abc"),
+			of("abc", 0, "-", "abc"),
+			of("abc", 2, "-", "abc")
+		);
+	}
+
 }
