56da209102a8f19185d785fc5f74e90c901df21d
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-05-25 22:24:13 +0900

    계약의 의한 테스트

---
+++ 3. Jpa/src/main/java/com/practice/jpa/reservation/Screening.java
@@ -0,0 +1,44 @@
+package com.practice.jpa.reservation;
+
+import java.time.LocalDateTime;
+
+/**
+ * 상영 클래스
+ *
+ * 영화 이름
+ * 순서
+ * 시간
+ * 
+ */
+public class Screening {
+	private Movie movie;
+	private int sequence;
+	private LocalDateTime whenScreened;
+
+	public Screening(Movie movie, int sequence, LocalDateTime whenScreened) {
+		this.movie = movie;
+		this.sequence = sequence;
+		this.whenScreened = whenScreened;
+	}
+
+	/**
+	 * 시작 시간
+	 */
+	public LocalDateTime getStartTime() {
+		return whenScreened;
+	}
+
+	/**
+	 * 순선 일치 여부
+	 */
+	public boolean isSequence(int sequence) {
+		return this.sequence == sequence;
+	}
+
+	/**
+	 * 영화 가격
+	 */
+	public Money getMovieFee() {
+		return movie.getFee();
+	}
+}
