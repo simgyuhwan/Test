16f37c7539a2562a07efc3e28ea8f1b4966719d1
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-06-27 15:25:07 +0900

    날짜 객체 모의테스트

--- 3. Jpa/src/test/java/com/practice/jpa/test/ChristmasDiscountTest.java
+++ 3. Jpa/src/test/java/com/practice/jpa/test/ChristmasDiscountTest.java
@@ -12,9 +12,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-/*
-	ChristmasDiscount 내부의 함수는 LocalDate.now(); 를 직접 호출한다. 외부에서 이 날짜를 제어하고 싶다면
-	날짜를 캡슐화하여 모키토의 기능을 이용하자.
+/**
+ * ChristmasDiscount 내부의 함수는 LocalDate.now(); 를 직접 호출한다. 외부에서 이 날짜를 제어하고 싶다면
+ * 	날짜를 캡슐화하여 모키토의 기능을 이용하자.
+ *
+ * 	* 소유하지 않는 것은 모의하지 말자.
+ * 	Clock 처럼 추상화 클래스를 만들어서 모의하자. 하지만 이렇게 할 경우, Clock이 추상화하고 있는 외부 라이브러리의 변화에는
+ * 	테스트 실패가 이루어지지 않는다. 이때는 통합 테스트를 활용하자. (테스트 분리)
+ *
  */
 @ExtendWith(MockitoExtension.class)
 class ChristmasDiscountTest {
