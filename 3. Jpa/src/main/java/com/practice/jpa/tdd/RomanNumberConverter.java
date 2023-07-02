dba9ef8f6f2a51126a304a5dcdc9626786927e62
simgyuhwan <rbghks19@naver.com>
2023-07-02 14:34:41 +0900

    practice tdd romanCoverter

--- 3. Jpa/src/main/java/com/practice/jpa/tdd/RomanNumberConverter.java
+++ 3. Jpa/src/main/java/com/practice/jpa/tdd/RomanNumberConverter.java
@@ -5,17 +5,34 @@ import java.util.Map;
 
 public class RomanNumberConverter {
 
-	private static Map<String, Integer> table = new HashMap<>() {{
-		put("I", 1);
-		put("V", 5);
-		put("X", 10);
-		put("L", 50);
-		put("C", 100);
-		put("D", 500);
-		put("M", 1000);
-	}};
+    private static Map<Character, Integer> table = new HashMap<>() {{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
+    }};
 
-	public int converter(String numberInRoman) {
-		return table.get(numberInRoman);
-	}
+    public int converter(String numberInRoman) {
+        int finalNumber = 0;
+        int lastNeighbor = 0;
+
+        for (int i = numberInRoman.length() - 1; i >= 0; i--) {
+            int current = table.get(numberInRoman.charAt(i));
+
+            int multiplier = 1;
+            if (current < lastNeighbor) {
+                multiplier = -1;
+            }
+
+            int currentNumberalToBeAdded = current * multiplier;
+            finalNumber += currentNumberalToBeAdded;
+
+            lastNeighbor = current;
+        }
+
+        return finalNumber;
+    }
 }
--- 3. Jpa/src/test/java/com/practice/jpa/tdd/RomanNumberConverterTest.java
+++ 3. Jpa/src/test/java/com/practice/jpa/tdd/RomanNumberConverterTest.java
@@ -8,27 +8,39 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RomanNumberConverterTest {
 
-	@Test
-	void shouldUnderstandSymbolI() {
-		RomanNumberConverter roman = new RomanNumberConverter();
-		int number = roman.converter("I");
-		assertThat(number).isEqualTo(1);
-	}
+    @ParameterizedTest
+    @CsvSource({"I,1", "V,5", "X,10", "L,50", "C, 100", "D, 500", "M, 1000"})
+    void shouldUnderstandOneCharNumbers(String romanNumber, int expectedNumberAfterConversion) {
+        RomanNumberConverter roman = new RomanNumberConverter();
+        int convertedNumber = roman.converter(romanNumber);
+        assertThat(convertedNumber).isEqualTo(expectedNumberAfterConversion);
+    }
 
-	@Test
-	void shouldUnderstandSymbolV() {
-		RomanNumberConverter roman = new RomanNumberConverter();
-		int number = roman.converter("V");
-		assertThat(number).isEqualTo(5);
-	}
+    // ver 1
+    @Test
+    void shouldUnderstandMultipleCharNumbers() {
+        RomanNumberConverter roman = new RomanNumberConverter();
+        int convertedNumber = roman.converter("II");
+        assertThat(convertedNumber).isEqualTo(2);
+    }
 
-	@ParameterizedTest
-	@CsvSource({"I,1", "V,5", "X,10", "L,50", "C, 100", "D, 500", "M, 1000"})
-	void shouldUnderstandOneCharNumbers(String romanNumber, int expectedNumberAfterConversion) {
-		RomanNumberConverter roman = new RomanNumberConverter();
-		int convertedNumber = roman.converter(romanNumber);
-		assertThat(convertedNumber).isEqualTo(expectedNumberAfterConversion);
-	}
+    // ver2
+    @ParameterizedTest
+    @CsvSource({"II, 2", "III, 3", "VI, 6", "XVIII, 18", "XXIII, 23", "DCCLXVI, 766"})
+    void shouldUnderstandMultipleCharNumbers(String romanNumber,
+        int expectedNumberAfterConversion) {
+        RomanNumberConverter roman = new RomanNumberConverter();
+        int convertedNumber = roman.converter(romanNumber);
+        assertThat(convertedNumber).isEqualTo(expectedNumberAfterConversion);
+    }
 
+    @ParameterizedTest
+    @CsvSource({"IV,4", "XIV,14", "XL,40", "XLI,41", "CCXCIV,294"})
+    void shouldUnderstandSubtractiveNotation(String romanNumber,
+        int expectedNumberAfterConversion) {
+        RomanNumberConverter roman = new RomanNumberConverter();
+        int convertedNumber = roman.converter(romanNumber);
+        assertThat(convertedNumber).isEqualTo(expectedNumberAfterConversion);
+    }
 }
 
