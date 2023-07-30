4b3357e9933e1f847becbef8a53012125d07e2f7
simgyuhwan <rbghks19@naver.com>
2023-07-30 17:33:41 +0900

    Controller 통합 테스트 통일

---
+++ 4. coffee-service/src/main/java/com/test/cafekiosk/spring/api/service/product/ProductNumberFactory.java
@@ -0,0 +1,23 @@
+package com.test.cafekiosk.spring.api.service.product;
+
+import com.test.cafekiosk.spring.domain.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ProductNumberFactory {
+    private final ProductRepository productRepository;
+
+    public String createNextProductNumber() {
+        String latestProductNumber = productRepository.findLatestProductNumber();
+        if (latestProductNumber == null) {
+            return "001";
+        }
+
+        int latestProductNumberInt = Integer.parseInt(latestProductNumber);
+        int nextProductNumberInt = latestProductNumberInt + 1;
+
+        return String.format("%03d", nextProductNumberInt);
+    }
+}
