d64f56133aae63ac3bbbefd43918a1d8ed97d80e
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-10-13 15:04:46 +0900

    init : 기본 설정 추가

---
+++ 6. manage/src/main/java/com/employees/manage/staff/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.employees.manage.staff.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Gyuhwan
+ */
+@RequestMapping("/api/v1")
+@RestController
+public class TestController {
+
+    @GetMapping("/only-test")
+    public String test() {
+        return "perfect test";
+    }
+
+}
