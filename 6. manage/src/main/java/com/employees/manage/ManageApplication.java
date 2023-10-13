69c0486184f35f3a1959838cbd40b99c62ee688c
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-10-13 14:49:05 +0900

    직원 관리 시스템 시작

--- 6. manage/src/main/java/com/employees/manage/ManageApplication.java
+++ 6. manage/src/main/java/com/employees/manage/ManageApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.employees.manage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ManageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ManageApplication.class, args);
 	}
 
 }
--- 6. manage/src/test/java/com/employees/manage/ManageApplicationTests.java
+++ 6. manage/src/test/java/com/employees/manage/ManageApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.employees.manage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ManageApplicationTests {
 
 	@Test
 	void contextLoads() {
